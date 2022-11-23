227adc24d6a07d3292f366f2123e724bae1804b1
Obarda <73034911+Obarda@users.noreply.github.com>
2022-11-24 01:33:11 +0200

    Update compress.py

--- compress.py
+++ compress.py
@@ -17,8 +17,6 @@ shutil.copyfile(glbfile, tempGlbFile)
 
 subprocess.run('glb-unpack ' + tempGlbFile, shell = True)
 
-print('Unpack...Done\n')
-
 ext = ('.png', '.jpg')
 
 for file in os.listdir(tempdirectory.name):
@@ -28,20 +26,12 @@ for file in os.listdir(tempdirectory.name):
         tmpKtxFile = os.path.splitext(tempImgFile)[0] + '.ktx'
         subprocess.run('toktx --t2 ' + tmpKtxFile + ' ' + tempImgFile)
         
-print('Fix ICC & Convert to KTX ...Done\n')
-
 os.remove(tempGlbFile)
-
-print('Delete temp GLB...Done\n')
 
 gltffile = os.path.splitext(tempGlbFile)[0] + '.gltf'
 
 subprocess.run('glb-pack ' + gltffile, shell = True)
 
-print('Pack to GLB...Done\n')
-
 shutil.copyfile(tempGlbFile, glbfile)
-
-print('Update GLB...Done\n')
 
 input('Press any key...')
